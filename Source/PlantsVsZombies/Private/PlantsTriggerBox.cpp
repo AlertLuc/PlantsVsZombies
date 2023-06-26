dc258edd1ab8f8de0964e6fc34fa818aab5f8790
Luc <2235615010@qq.com>
2023-06-26 19:37:09 +0800

    feat: Solar generation

--- Source/PlantsVsZombies/Private/PlantsTriggerBox.cpp
+++ Source/PlantsVsZombies/Private/PlantsTriggerBox.cpp
@@ -5,16 +5,45 @@
 #include "Plants2DCharacter.h"
 #include "Zombies2DCharacter.h"
 
+void APlantsTriggerBox::NotifyPlants() const
+{
+	if (HaveZombiesNum > 0)
+	{
+		CanAttackZombiesEvent.Broadcast(true);
+	}
+	else
+	{
+		CanAttackZombiesEvent.Broadcast(false);
+	}
+}
+
+void APlantsTriggerBox::NotifyZombies() const
+{
+	if (HavePlantsNum > 0)
+	{
+		CanAttackPlantsEvent.Broadcast(true);
+	}
+	else
+	{
+		CanAttackPlantsEvent.Broadcast(false);
+	}
+}
+
 void APlantsTriggerBox::NotifyActorBeginOverlap(AActor* OtherActor)
 {
 	if(APlants2DCharacter* Plants2DCharacter = Cast<APlants2DCharacter>(OtherActor))
 	{
+		HavePlantsNum++;
+		NotifyZombies();
+		NotifyPlants();
 		// 注册事件
 		Plants2DCharacter->CanAttackZombiesDelegateHandle = CanAttackZombiesEvent.AddUObject(Plants2DCharacter, &APlants2DCharacter::HandleCanAttackZombies);
 	}
 	if(Cast<AZombies2DCharacter>(OtherActor))
 	{
-		CanAttackZombiesEvent.Broadcast(true);
+		HaveZombiesNum++;
+		NotifyZombies();
+		NotifyPlants();
 	}
 	Super::NotifyActorBeginOverlap(OtherActor);
 }
@@ -23,12 +52,17 @@ void APlantsTriggerBox::NotifyActorEndOverlap(AActor* OtherActor)
 {
 	if (const APlants2DCharacter* Plants2DCharacter = Cast<APlants2DCharacter>(OtherActor))
 	{
+		HavePlantsNum--;
+		NotifyZombies();
+		NotifyPlants();
 		// 取消注册事件
 		CanAttackZombiesEvent.Remove(Plants2DCharacter->CanAttackZombiesDelegateHandle);
 	}
 	if (Cast<AZombies2DCharacter>(OtherActor))
 	{
-		CanAttackZombiesEvent.Broadcast(false);
+		HaveZombiesNum--;
+		NotifyZombies();
+		NotifyPlants();
 	}
 	Super::NotifyActorEndOverlap(OtherActor);
 }
--- Source/PlantsVsZombies/Public/PlantsTriggerBox.h
+++ Source/PlantsVsZombies/Public/PlantsTriggerBox.h
@@ -15,7 +15,11 @@ class PLANTSVSZOMBIES_API APlantsTriggerBox : public ATriggerBox
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+		int HaveZombiesNum = 0;
 
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+		int HavePlantsNum = 0;
 
 	/* 重写函数 */
 
@@ -23,6 +27,11 @@ public:
 
 	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
 
+
+	/* 事件 */
+	void NotifyPlants() const;
+
+	void NotifyZombies() const;
 private:
 
 
@@ -30,4 +39,9 @@ private:
 
 	DECLARE_EVENT_OneParam(APlants2DCharacter, CanAttackZombies, bool)
 	CanAttackZombies CanAttackZombiesEvent;
+
+	DECLARE_EVENT_OneParam(AZombies2DCharacter, CanAttackPlants, bool)
+	CanAttackPlants CanAttackPlantsEvent;
 };
+
+
