6540f6f0b866a000d147afbe57f1b6e90119ba4e
Luc <2235615010@qq.com>
2023-07-12 01:08:32 +0800

    WIP

--- Source/PlantsVsZombies/Private/Sun2DCharacter.cpp
+++ Source/PlantsVsZombies/Private/Sun2DCharacter.cpp
@@ -2,4 +2,59 @@
 
 
 #include "Sun2DCharacter.h"
+#include "Kismet/KismetMathLibrary.h"
 
+void ASun2DCharacter::SunMoveTick()
+{
+	auto Position = GetActorLocation();
+	if(!bIsFirstMove)
+	{
+		bIsFirstMove = true;
+		Position.Z = 0.;
+		TargetDirection = UKismetMathLibrary::GetDirectionUnitVector(Position, TargetPosition);
+	}
+	AddMovementInput(TargetDirection);
+	if(UKismetMathLibrary::NearlyEqual_FloatFloat(Position.X, TargetPosition.X, 10.) &&
+		UKismetMathLibrary::NearlyEqual_FloatFloat(Position.Y, TargetPosition.Y, 10.))
+	{
+		Destroy();
+		SunAdd();
+	}
+}
+
+void ASun2DCharacter::SunFullTick()
+{
+	AddMovementInput(GetActorRightVector());
+}
+
+void ASun2DCharacter::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	if(MoveTimer.IsValid())
+	{
+		GWorld->GetTimerManager().ClearTimer(MoveTimer);
+	}
+	if (FullTimer.IsValid())
+	{
+		GWorld->GetTimerManager().ClearTimer(FullTimer);
+	}
+}
+
+void ASun2DCharacter::NotifyActorOnClicked(FKey ButtonPressed)
+{
+	Super::NotifyActorOnClicked(ButtonPressed);
+	GWorld->GetTimerManager().SetTimer(MoveTimer, this, &ASun2DCharacter::SunMoveTick, 0.01, true);
+}
+
+void ASun2DCharacter::SunAdd_Implementation()
+{
+}
+
+void ASun2DCharacter::SunFull(bool bIsFull)
+{
+	if (bIsFull) 
+	{
+		GWorld->GetTimerManager().SetTimer(FullTimer, this, &ASun2DCharacter::SunFullTick, 0.01, true);
+	}
+	GWorld->GetTimerManager().ClearTimer(FullTimer);
+}
--- Source/PlantsVsZombies/Public/Sun2DCharacter.h
+++ Source/PlantsVsZombies/Public/Sun2DCharacter.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "PaperZDCharacter.h"
-#include "GameFramework/CharacterMovementComponent.h"
 #include "Sun2DCharacter.generated.h"
 
 /**
@@ -15,6 +14,27 @@ class PLANTSVSZOMBIES_API ASun2DCharacter : public APaperZDCharacter
 {
 	GENERATED_BODY()
 
+	void SunMoveTick();
+	void SunFullTick();
+
 public:
 
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void NotifyActorOnClicked(FKey ButtonPressed) override;
+
+	UFUNCTION(BlueprintNativeEvent)
+	void SunAdd();
+
+	void SunAdd_Implementation();
+
+	UFUNCTION(BlueprintCallable)
+	void SunFull(bool bIsFull);
+
+private:
+	bool bIsFirstMove = false;
+	FVector TargetDirection{};
+	const FVector TargetPosition{ -480.,-220.,0. };
+
+	FTimerHandle MoveTimer{};
+	FTimerHandle FullTimer{};
 };
